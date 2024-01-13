259078e145efb06eadfd640901109f53b7b12193
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-01-13 23:34:38 +0000

    pass in main file

--- src/main/java/net/slqmy/parrot_mail/ParrotMailPlugin.java
+++ src/main/java/net/slqmy/parrot_mail/ParrotMailPlugin.java
@@ -2,7 +2,6 @@ package net.slqmy.parrot_mail;
 
 import net.slqmy.parrot_mail.event.ParrotRightClickListener;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,6 +11,6 @@ public final class ParrotMailPlugin extends JavaPlugin {
     public void onEnable() {
         PluginManager pluginManager = Bukkit.getPluginManager();
 
-        pluginManager.registerEvents(new ParrotRightClickListener(), this);
+        pluginManager.registerEvents(new ParrotRightClickListener(this), this);
     }
 }
