4aa708aa6bf5fd94bef7a6a1f39c48c29a72d8c0
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-01-17 20:41:02 +0000

    Translating bundle

--- src/main/java/net/slqmy/parrot_mail/MailParrotUtils.java
+++ src/main/java/net/slqmy/parrot_mail/MailParrotUtils.java
@@ -6,13 +6,36 @@ import org.bukkit.Location;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.ItemDisplay;
 import org.bukkit.entity.Parrot;
+import org.bukkit.util.Transformation;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
+import org.joml.Vector3f;
+import org.joml.Vector3fc;
 
 import java.util.logging.Level;
 
 public class MailParrotUtils {
     public static void updateBundlePosition(@NotNull Parrot parrot, @NotNull ItemDisplay bundle) {
-        bundle.teleport(parrot.getLocation().add(0, 1, 0));
+        Location parrotLocation = parrot.getLocation();
+        Location bundleLocation = bundle.getLocation();
+
+        Vector forward = parrotLocation.getDirection();
+        Vector left = forward.clone().rotateAroundY(Math.PI / 2);
+        Vector up = forward.clone().crossProduct(left);
+
+        Transformation transformation = bundle.getTransformation();
+
+        Vector3f parrotTranslation = new Vector3f(
+                (float) (parrotLocation.getX() - bundleLocation.getX()),
+                (float) (parrotLocation.getY() - bundleLocation.getY()),
+                (float) (parrotLocation.getZ() - bundleLocation.getZ())
+        );
+
+        parrotTranslation.add(new Vector3f((float) up.getX(), (float) up.getY(), (float) up.getZ()).mul(0.4F));
+        parrotTranslation.add(new Vector3f((float) forward.getX(), (float) forward.getY(), (float) forward.getZ()).mul(0.3F));
+
+        transformation.getTranslation().set(parrotTranslation);
+
+        bundle.setTransformation(transformation);
     }
 }
